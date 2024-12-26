7a398357b3a74c0177b84632bbbcbbf0ed302247
Pyae Sone Shin Thant <pssthant@gmail.com>
2024-12-27 00:20:08 +0700

    feat(nvim): update config files

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,24 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.opt.number = true
+vim.opt.title = true
+vim.opt.showcmd = true
+vim.opt.cmdheight = 1
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.ignorecase = true
+vim.opt.smarttab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+-- Disable mouse
+vim.opt.mouse = ""
+
+-- Undercurl
+vim.cmd([[let &t_Cs = "\e[4:3m"]])
+vim.cmd([[let &t_Ce = "\e[4:0m"]])
+
+-- Add asteriks in block comments
+vim.opt.formatoptions:append({ "r" })
