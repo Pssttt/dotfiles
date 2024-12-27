fbcebffc40c73ada366e9caf844ca71fd99c755e
Pyae Sone Shin Thant <pssthant@gmail.com>
2024-12-27 21:40:35 +0700

    feat(nvim): add discipline lua

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local discipline = require("psst.discipline")
+
+discipline.cowboy()
+
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
@@ -12,7 +16,7 @@ keymap.set("n", "-", "<C-x>")
 keymap.set("n", "dw", "vb_d")
 
 -- Select all
-keymap.set("n", "<C-a", "gg<S-v>G")
+keymap.set("n", "<C-a>", "gg<S-v>G", opts)
 
 -- Jumplist
 keymap.set("n", "<C-m>", "<C-i>", opts)
---
+++ nvim/.config/nvim/lua/psst/discipline.lua
@@ -0,0 +1,37 @@
+local M = {}
+
+function M.cowboy()
+  local ok = true
+  for _, key in ipairs({ "h", "j", "k", "l" }) do
+    local count = 0
+    local timer = vim.loop.new_timer()
+    vim.keymap.set("n", key, function()
+      if vim.v.count > 0 then
+        count = 0
+      end
+      if count >= 10 and vim.bo.buftype ~= "nofile" then
+        ok = pcall(vim.notify, "Hold it there!", vim.log.levels.WARN, {
+          icon = "🤠",
+          id = "cowboy",
+          keep = function()
+            return count >= 10
+          end,
+        })
+        if not ok then
+          return key
+        end
+      else
+        count = count + 1
+        if timer:is_active() then
+          timer:stop()
+        end
+        timer:start(2000, 0, function()
+          count = 0
+        end)
+        return key
+      end
+    end, { expr = true, silent = true })
+  end
+end
+
+return M
