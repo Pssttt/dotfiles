339be0183d33ce97094a86cb769ce68cfacf7022
Pyae Sone Shin Thant <pssthant@gmail.com>
2024-12-28 00:36:09 +0700

    feat(fish): update fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,4 +2,21 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set fish_greeting ""
+
+set -gx TERM xterm-256color
+
+# aliases
+alias g git
+alias lg lazygit
+alias sp spotify_player
+alias v nvim
+alias ls eza
+alias la "eza -A"
+alias ll "eza -l --icons"
+alias lla "eza -l -A --icons"
+
+set -gx EZA_CONFIG_DIR /Users/psst/.config/eza
+set -gx XDG_CONFIG_HOME /Users/psst/.config
+
 fish_add_path /Users/psst/.spicetify
--- fish/.config/fish/functions/sp.fish
+++
@@ -1,4 +0,0 @@
-function sp --wraps=spotify_player --description 'alias sp=spotify_player'
-    spotify_player $argv
-
-end
