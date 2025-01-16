0575cade8c9b0dca48fa6c86946ea014339a556e
Pyae Sone Shin Thant <pssthant@gmail.com>
2025-01-16 09:39:07 +0700

    feat(wezterm): update leader key and add keymappings for resize & move

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -5,24 +5,33 @@ local custom = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
 custom.background = "#191724"
 custom.cursor_bg = "#bac2de"
 
-config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 1000 }
+config.leader = { key = "Space", mods = "SHIFT", timeout_milliseconds = 1000 }
 config.keys = {
 	-- splitting
-	{ mods = "LEADER", key = "-", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
-	{ mods = "LEADER", key = "/", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ mods = "LEADER", key = "s", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ mods = "LEADER", key = "v", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
 	{ mods = "LEADER", key = "LeftArrow", action = wezterm.action.ActivatePaneDirection("Left") },
-	{ mods = "LEADER", key = "h", action = wezterm.action.ActivatePaneDirection("Left") },
+	{ mods = "SUPER | SHIFT", key = "h", action = wezterm.action.ActivatePaneDirection("Left") },
 	{ mods = "LEADER", key = "RightArrow", action = wezterm.action.ActivatePaneDirection("Right") },
-	{ mods = "LEADER", key = "l", action = wezterm.action.ActivatePaneDirection("Right") },
+	{ mods = "SUPER | SHIFT", key = "l", action = wezterm.action.ActivatePaneDirection("Right") },
 	{ mods = "LEADER", key = "UpArrow", action = wezterm.action.ActivatePaneDirection("Up") },
-	{ mods = "LEADER", key = "k", action = wezterm.action.ActivatePaneDirection("Up") },
+	{ mods = "SUPER | SHIFT", key = "k", action = wezterm.action.ActivatePaneDirection("Up") },
 	{ mods = "LEADER", key = "DownArrow", action = wezterm.action.ActivatePaneDirection("Down") },
-	{ mods = "LEADER", key = "j", action = wezterm.action.ActivatePaneDirection("Down") },
-	{ mods = "LEADER", key = "p", action = wezterm.action.ActivateTabRelative(-1) },
-	{ mods = "LEADER", key = "n", action = wezterm.action.ActivateTabRelative(1) },
-	{ mods = "LEADER", key = "x", action = wezterm.action.CloseCurrentPane({ confirm = false }) },
-	{ mods = "LEADER", key = "X", action = wezterm.action.CloseCurrentTab({ confirm = false }) },
-	{ mods = "LEADER", key = "c", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+	{ mods = "SUPER | SHIFT", key = "j", action = wezterm.action.ActivatePaneDirection("Down") },
+	{ mods = "SUPER | SHIFT", key = "p", action = wezterm.action.ActivateTabRelative(-1) },
+	{ mods = "SUPER | SHIFT", key = "n", action = wezterm.action.ActivateTabRelative(1) },
+	{ mods = "SUPER | SHIFT", key = "x", action = wezterm.action.CloseCurrentPane({ confirm = false }) },
+	{ mods = "SUPER | SHIFT", key = "c", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+	{
+		mods = "LEADER",
+		key = "r",
+		action = wezterm.action.ActivateKeyTable({ name = "resize_pane", one_shot = false }),
+	},
+	{
+		mods = "LEADER",
+		key = "m",
+		action = wezterm.action.ActivateKeyTable({ name = "move_tab", one_shot = false }),
+	},
 	{
 		mods = "LEADER",
 		key = "!",
@@ -41,9 +50,26 @@ for i = 1, 9 do
 	})
 end
 
-config.color_scheme = "Mikeppuccin"
+config.key_tables = {
+	resize_pane = {
+		{ key = "h", action = wezterm.action.AdjustPaneSize({ "Left", 1 }) },
+		{ key = "j", action = wezterm.action.AdjustPaneSize({ "Down", 1 }) },
+		{ key = "k", action = wezterm.action.AdjustPaneSize({ "Up", 1 }) },
+		{ key = "l", action = wezterm.action.AdjustPaneSize({ "Right", 1 }) },
+		{ key = "Escape", action = "PopKeyTable" },
+		{ key = "Enter", action = "PopKeyTable" },
+	},
+	move_tab = {
+		{ key = "H", action = wezterm.action.MoveTabRelative(-1) },
+		{ key = "L", action = wezterm.action.MoveTabRelative(1) },
+		{ key = "Escape", action = "PopKeyTable" },
+		{ key = "Enter", action = "PopKeyTable" },
+	},
+}
+
+config.color_scheme = "Psstuccin"
 config.color_schemes = {
-	["Mikeppuccin"] = custom,
+	["Psstuccin"] = custom,
 }
 config.font = wezterm.font({ family = "Hack Nerd Font" })
 config.font_size = 16
@@ -55,7 +81,7 @@ config.macos_window_background_blur = 40
 config.send_composed_key_when_left_alt_is_pressed = true -- MacOS Fix
 config.use_fancy_tab_bar = false
 config.warn_about_missing_glyphs = false
-config.window_background_opacity = 0.94
+config.window_background_opacity = 0.90
 config.window_close_confirmation = "NeverPrompt"
 config.window_decorations = "RESIZE"
 config.window_padding = {
