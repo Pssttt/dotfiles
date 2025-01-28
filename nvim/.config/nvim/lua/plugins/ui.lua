20ffcfb114215e328b708460d1bc1a3f3476e904
Pyae Sone Shin Thant <pssthant@gmail.com>
2025-01-28 15:24:57 +0700

    feat(nvim): add two sections to nvim dashboard

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -123,16 +123,27 @@ return {
 			dashboard = {
 				preset = {
 					header = [[
-                        
+
 
 ██████╗ ███████╗███████╗████████╗
 ██╔══██╗██╔════╝██╔════╝╚══██╔══╝
 ██████╔╝███████╗███████╗   ██║   
 ██╔═══╝ ╚════██║╚════██║   ██║   
 ██║     ███████║███████║   ██║   
-╚═╝     ╚══════╝╚══════╝   ╚═╝   
-                                
-                    ]],
+╚═╝     ╚══════╝╚══════╝   ╚═╝   ]],
+				},
+				sections = {
+					{ section = "header" },
+					{ icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+					{
+						icon = " ",
+						title = "Recent Files",
+						section = "recent_files",
+						indent = 2,
+						padding = 1,
+					},
+					{ icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+					{ section = "startup" },
 				},
 			},
 		},
