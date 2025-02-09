314ee13413709820a85914beafc199d1813f8b16
Pyae Sone Shin Thant <pssthant@gmail.com>
2025-02-09 18:36:44 +0700

    feat(nvim): update dial.nvim for more elems

--- nvim/.config/nvim/lua/plugins/coding.lua
+++ nvim/.config/nvim/lua/plugins/coding.lua
@@ -1,5 +1,4 @@
 return {
-	-- Refactoring tool
 	{
 		"danymat/neogen",
 		keys = {
@@ -49,18 +48,154 @@ return {
 				desc = "Decrement",
 			},
 		},
-		config = function()
+		opts = function()
 			local augend = require("dial.augend")
-			require("dial.config").augends:register_group({
-				default = {
-					augend.integer.alias.decimal,
-					augend.integer.alias.hex,
-					augend.date.alias["%Y/%m/%d"],
-					augend.constant.alias.bool,
-					augend.semver.alias.semver,
-					augend.constant.new({ elements = { "let", "const" } }),
-				},
+
+			local logical_alias = augend.constant.new({
+				elements = { "&&", "||" },
+				word = false,
+				cyclic = true,
 			})
+
+			local ordinal_numbers = augend.constant.new({
+				elements = {
+					"first",
+					"second",
+					"third",
+					"fourth",
+					"fifth",
+					"sixth",
+					"seventh",
+					"eighth",
+					"ninth",
+					"tenth",
+				},
+				word = false,
+				cyclic = true,
+			})
+
+			local weekdays = augend.constant.new({
+				elements = {
+					"Monday",
+					"Tuesday",
+					"Wednesday",
+					"Thursday",
+					"Friday",
+					"Saturday",
+					"Sunday",
+				},
+				word = true,
+				cyclic = true,
+			})
+
+			local months = augend.constant.new({
+				elements = {
+					"January",
+					"February",
+					"March",
+					"April",
+					"May",
+					"June",
+					"July",
+					"August",
+					"September",
+					"October",
+					"November",
+					"December",
+				},
+				word = true,
+				cyclic = true,
+			})
+
+			local capitalized_boolean = augend.constant.new({
+				elements = {
+					"True",
+					"False",
+				},
+				word = true,
+				cyclic = true,
+			})
+
+			return {
+				dials_by_ft = {
+					css = "css",
+					vue = "vue",
+					javascript = "typescript",
+					typescript = "typescript",
+					typescriptreact = "typescript",
+					javascriptreact = "typescript",
+					json = "json",
+					lua = "lua",
+					markdown = "markdown",
+					sass = "css",
+					scss = "css",
+					python = "python",
+				},
+				groups = {
+					default = {
+						augend.integer.alias.decimal,
+						augend.constant.new({ elements = { "public", "private" }, cyclic = true }),
+						augend.integer.alias.decimal_int,
+						augend.integer.alias.hex,
+						augend.date.alias["%Y/%m/%d"],
+						ordinal_numbers,
+						weekdays,
+						months,
+						capitalized_boolean,
+						augend.constant.alias.bool,
+						logical_alias,
+					},
+					vue = {
+						augend.constant.new({ elements = { "let", "const" } }),
+						augend.hexcolor.new({ case = "lower" }),
+						augend.hexcolor.new({ case = "upper" }),
+					},
+					typescript = {
+						augend.constant.new({ elements = { "let", "const" } }),
+					},
+					css = {
+						augend.hexcolor.new({
+							case = "lower",
+						}),
+						augend.hexcolor.new({
+							case = "upper",
+						}),
+					},
+					markdown = {
+						augend.constant.new({
+							elements = { "[ ]", "[x]" },
+							word = false,
+							cyclic = true,
+						}),
+						augend.misc.alias.markdown_header,
+					},
+					json = {
+						augend.semver.alias.semver, -- versioning (v1.1.2)
+					},
+					lua = {
+						augend.constant.new({
+							elements = { "and", "or" },
+							word = true, -- if false, "sand" is incremented into "sor", "doctor" into "doctand", etc.
+							cyclic = true, -- "or" is incremented into "and".
+						}),
+					},
+					python = {
+						augend.constant.new({
+							elements = { "and", "or" },
+						}),
+					},
+				},
+			}
+		end,
+		config = function(_, opts)
+			-- copy defaults to each group
+			for name, group in pairs(opts.groups) do
+				if name ~= "default" then
+					vim.list_extend(group, opts.groups.default)
+				end
+			end
+			require("dial.config").augends:register_group(opts.groups)
+			vim.g.dials_by_ft = opts.dials_by_ft
 		end,
 	},
 }
