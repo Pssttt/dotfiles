3e1eab2189adcadbd16ae1047a35353f3e2e4d29
Pyae Sone Shin Thant <pssthant@gmail.com>
2025-09-26 20:56:47 +0700

    refactor(colorscheme.lua): remove unnecessary config function in colorscheme plugin.

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -7,12 +7,5 @@ return {
 		opts = {
 			transparent_background = true,
 		},
-		config = function(_, opts)
-			require("catppuccin").setup(opts)
-			local module = require("catppuccin.groups.integrations.bufferline")
-			if module then
-				module.get = module.get_theme
-			end
-		end,
 	},
 }
